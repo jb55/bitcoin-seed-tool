f4987dcabf197607ec2c37443d0d4a1f8c24874f
William Casarin <jb55@jb55.com>
2020-02-16 12:53:53 -0800

    dice test

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ mod descriptor;
 
 use bip39::{Mnemonic, MnemonicType, Language, Seed};
 
-use bitcoin_hashes::sha256::Hash as sha256;
+use bitcoin_hashes::Hash;
+use bitcoin_hashes::sha256::Hash as Sha256;
 
 fn main() {
 
-    // create a new randomly generated mnemonic phrase
-    let mnemonic = Mnemonic::from_entropy(b"1234", Language::English).unwrap();
+    // example diceroll entropy
+    let entropy : Sha256 = Hash::hash(b"1234561234561346513412341436514356513246536543256421653");
+    println!("entropy: {:x}", &entropy);
+
+    let mnemonic = Mnemonic::from_entropy(&entropy, Language::English).unwrap();
 
     // get the phrase
     let phrase: &str = mnemonic.phrase();
@@ -18,7 +22,7 @@ fn main() {
     let seed = Seed::new(&mnemonic, "");
 
     // get the HD wallet seed as raw bytes
-    let seed_bytes: &[u8] = seed.as_bytes();
+    // let seed_bytes: &[u8] = seed.as_bytes();
 
     // print the HD wallet seed as a hex string
     println!("{:X}", seed);
